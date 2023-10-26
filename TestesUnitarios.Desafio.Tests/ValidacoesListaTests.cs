427da43c4f7e8d9e453590753d44b8f005ac98be
Marcelo Hoffmann <hoffmannmarcelo@hotmail.com>
2023-10-26 10:23:07 -0300

    Realizado desafio

--- TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
+++ TestesUnitarios.Desafio.Tests/ValidacoesListaTests.cs
@@ -44,22 +44,36 @@ public class ValidacoesListaTests
         var numeroParaProcurar = 10;
 
         // Act
+        var resultado = _validacoes.ListaContemDeterminadoNumero(lista, numeroParaProcurar);
 
-        // Assert
+        // Assert]
+
+        Assert.False(resultado);
+
     }
 
     //TODO: Corrigir a anotação [Fact]
+
+    [Fact]
     public void DeveMultiplicarOsElementosDaListaPor2()
     {
         //TODO: Implementar método de teste
 
         // Arrange
         var lista = new List<int> { 5, 7, 8, 9 };
+
         var resultadoEsperado = new List<int> { 10, 14, 16, 18 };
-        
+
         // Act
+        for (int i = 0; i < lista.Count; i++)
+        {
+            lista[i] *= 2;
+        }
+
 
         // Assert
+        Assert.Equal(resultadoEsperado, lista);
+
     }
 
     [Fact]
@@ -71,11 +85,30 @@ public class ValidacoesListaTests
         var lista = new List<int> { 5, -1, -8, 9 };
 
         // Act
+        int maiorNumero = EncontrarMaiorNumero(lista);
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(9, 9);
+        Assert.Equal(9, maiorNumero);
+
+
     }
+
+    private int EncontrarMaiorNumero(List<int> lista)
+    {
+        int maior = int.MinValue; // Inicializa com o menor valor possível
+
+        foreach (var numero in lista)
+        {
+            if (numero > maior)
+            {
+                maior = numero;
+            }
+        }
+
+        return maior;
+    }
+
 
     [Fact]
     public void DeveRetornarOitoNegativoComoMenorNumeroDaLista()
@@ -90,6 +123,6 @@ public class ValidacoesListaTests
 
         // Assert
         //TODO: Corrigir o Assert.Equal com base no retorno da chamada ao método
-        Assert.Equal(-8, -8);
+        Assert.Equal(-8, resultado);
     }
 }
